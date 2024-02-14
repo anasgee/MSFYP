dbd7e0c716ded23d1effd512b906eeb410f9ba79
Anas Raza <anasraza6096@gmail.com>
2024-02-14 17:54:52 +0500

    files added to repository

---
+++ fypms-main_4/fypms-main/src/Components/Classes/Class.js
@@ -0,0 +1,77 @@
+import { Link } from "react-router-dom";
+import { MdOutlineDeleteForever } from "react-icons/md";
+import Button from "../UI/Button";
+import CustomCard from "../UI/CustomCard";
+import classes from "./Class.module.css";
+
+const Class = (props) => {
+  const deleteClassHandler = (id) => {
+    props.onDeleteClass(id);
+  };
+
+  return (
+  //   <div className={classes.container}>
+  //     <Link to={`./${props.item.id}`}>
+  //       <CustomCard>
+  //         <div className={classes.class}>
+  //           <p className={classes.headline}>{props.item.name}</p>
+  //           <p className={classes.item}>
+  //             Total Students:<span>{props.item.totalStudents}</span>
+  //           </p>
+  //           <p className={classes.item}>
+  //             Total Projects:<span>{props.item.totalProjects}</span>
+  //           </p>
+  //           <p className={classes.item}>
+  //             Total Supervisors:<span>{props.item.assignedSupervisors}</span>
+  //           </p>
+  //         </div>
+  //       </CustomCard>
+  //     </Link>
+  //     <div className={classes.buttons}>
+  //       <MdOutlineDeleteForever
+  //         className={classes.icon}
+  //         onClick={() => deleteClassHandler(props.item.id)}
+  //       />
+  //       <Button onClick={props.passHandler}>Pass Class</Button>
+  //     </div>
+  //   </div>
+  // );
+
+
+
+
+
+  // ---------------------CHer Chaar
+
+
+
+
+    <div className={classes.container}>
+      {/* <Link to={`./${props.item.id}`}> */}
+        <CustomCard>
+          <div className={classes.class}>
+            <p className={classes.headline}>Bs It 8 <sup>th</sup> Evening</p>
+            <p className={classes.item}>
+              Total Students:<span>50</span>
+            </p>
+            <p className={classes.item}>
+              Total Projects:<span>10</span>
+            </p>
+            <p className={classes.item}>
+              Total Supervisors:<span>10</span>
+            </p>
+          </div>
+        </CustomCard>
+      {/* </Link> */}
+      <div className={classes.buttons}>
+        <MdOutlineDeleteForever
+          // className={classes.icon}
+          // onClick={() => deleteClassHandler(props.item.id)}
+        />
+        <Button onClick={props.passHandler}>Pass Class</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Class;
